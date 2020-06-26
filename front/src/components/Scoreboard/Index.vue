7a4397d7779c6d16d2a4bfc4798372774fe54f28
Lucas Druschke <ldruschk@posteo.de>
2020-06-26 22:09:33 +0200

    remove double minus sign for def points (closes #17)

--- front/src/components/Scoreboard/Index.vue
+++ front/src/components/Scoreboard/Index.vue
@@ -99,7 +99,7 @@
                         </div>
                         <div class="defense">
                             <i class="fas fa-shield-alt" />
-                            -{{ defense.toFixed(2) }}
+                            {{ defense.toFixed(2) }}
                         </div>
                     </div>
                 </div>
--- front/src/components/TeamScoreboard/Index.vue
+++ front/src/components/TeamScoreboard/Index.vue
@@ -58,7 +58,7 @@
                         </div>
                         <div class="defense">
                             <i class="fas fa-shield-alt" />
-                            -{{ defense.toFixed(2) }}
+                            {{ defense.toFixed(2) }}
                         </div>
                     </div>
                 </div>
