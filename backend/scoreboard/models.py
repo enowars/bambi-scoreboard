8856fc66810a05bc46fc1e245aa2671753df02ae
Lucas Druschke <ldruschk@posteo.de>
2020-11-13 12:15:52 +0100

    update watch.py to match new JSON key camel casing

--- backend/scoreboard/models.py
+++ backend/scoreboard/models.py
@@ -4,51 +4,51 @@ from pydantic import BaseModel
 
 
 class ServiceDetail(BaseModel):
-    ServiceId: int
-    AttackPoints: float
-    LostDefensePoints: float
-    ServiceLevelAgreementPoints: float
-    ServiceStatus: str
-    Message: Optional[str]
+    serviceId: int
+    attackPoints: float
+    lostDefensePoints: float
+    serviceLevelAgreementPoints: float
+    serviceStatus: str
+    message: Optional[str]
 
 
 class ScoreboardTeam(BaseModel):
-    Name: str
-    TeamId: int
-    TotalPoints: float
-    AttackPoints: float
-    LostDefensePoints: float
-    ServiceLevelAgreementPoints: float
-    ServiceDetails: List[ServiceDetail]
+    name: str
+    teamId: int
+    totalPoints: float
+    attackPoints: float
+    lostDefensePoints: float
+    serviceLevelAgreementPoints: float
+    serviceDetails: List[ServiceDetail]
 
 
 class FirstBlood(BaseModel):
-    TeamId: int
-    Timestamp: str
-    RoundId: int
-    StoreDescription: Optional[str]
-    StoreIndex: int
+    teamId: int
+    timestamp: str
+    roundId: int
+    storeDescription: Optional[str]
+    storeIndex: int
 
 
 class ScoreboardService(BaseModel):
-    ServiceId: int
-    ServiceName: str
-    MaxStores: int
-    FirstBloods: List[FirstBlood]
+    serviceId: int
+    serviceName: str
+    maxStores: int
+    firstBloods: List[FirstBlood]
 
 
 class JsonScoreboard(BaseModel):
-    CurrentRound: Optional[int]
-    StartTimeEpoch: Optional[int]
-    EndTimeEpoch: Optional[int]
-    Services: List[ScoreboardService]
-    Teams: List[ScoreboardTeam]
+    currentRound: Optional[int]
+    startTimeEpoch: Optional[int]
+    endTimeEpoch: Optional[int]
+    services: List[ScoreboardService]
+    teams: List[ScoreboardTeam]
 
 
 class TeamState(BaseModel):
-    Round: int
-    TotalPoints: float
-    AttackPoints: float
-    LostDefensePoints: float
-    ServiceLevelAgreementPoints: float
-    ServiceDetails: List[ServiceDetail]
+    round: int
+    totalPoints: float
+    attackPoints: float
+    lostDefensePoints: float
+    serviceLevelAgreementPoints: float
+    serviceDetails: List[ServiceDetail]
--- backend/scoreboard/watch.py
+++ backend/scoreboard/watch.py
@@ -18,7 +18,7 @@ class RestartedException(Exception):
 async def main() -> None:
     while True:
         try:
-            if (not await parse_info(os.path.join(DATA_DIR, "scoreboardInfo.json"))):
+            if not await parse_info(os.path.join(DATA_DIR, "scoreboardInfo.json")):
                 await asyncio.sleep(1)
                 continue
 
@@ -77,7 +77,7 @@ async def parse_base_scoreboard(base_path: str) -> None:
     except RestartedException:
         print("CTF was restarted, flushing Redis")
         await redis.flushall()
-        await parse_ctf(CTF_JSON_PATH)
+        await parse_info(os.path.join(DATA_DIR, "scoreboardInfo.json"))
         await parse_scoreboard(base_path)
         prev_max = 0
 
@@ -96,29 +96,29 @@ async def parse_scoreboard(file_: str) -> None:
     try:
         obj = json.load(open(file_, "r"))
         sb = JsonScoreboard(**obj)
-        print(f"Loading scoreboard for round {sb.CurrentRound}")
+        print(f"Loading scoreboard for round {sb.currentRound}")
 
-        if not sb.CurrentRound:
+        if not sb.currentRound:
             return
 
-        entry = await redis.get(f"sb_{sb.CurrentRound}")
+        entry = await redis.get(f"sb_{sb.currentRound}")
         if not entry:
-            await redis.set(f"sb_{sb.CurrentRound}", sb.json())
+            await redis.set(f"sb_{sb.currentRound}", sb.json())
 
         entry = await redis.get("max_round")
         print(f"previous: max_round: {entry}")
-        if not entry or int(entry.decode()) < sb.CurrentRound:
-            await redis.set("max_round", sb.CurrentRound)
-            await redis.publish("notifications", sb.CurrentRound)
+        if not entry or int(entry.decode()) < sb.currentRound:
+            await redis.set("max_round", sb.currentRound)
+            await redis.publish("notifications", sb.currentRound)
         elif (
             entry
-            and int(entry.decode()) > sb.CurrentRound
+            and int(entry.decode()) > sb.currentRound
             and basename == "scoreboard.json"
         ):
             raise RestartedException()
 
-        for t in sb.Teams:
-            await redis.set(f"team_exists_${t.TeamId}", True)
+        for t in sb.teams:
+            await redis.set(f"team_exists_${t.teamId}", True)
     except FileNotFoundError as e:
         print(f"Failed to load scoreboard: {file_}, {str(e)}")
     except json.JSONDecodeError as e:
